1a924ead5e04b90dbb9985bf717b95ccb2e61c34
sushil <sushilpandey80@gmail.com>
2019-09-30 20:05:16 +0530

    added

---
+++ src/test/java/Maven/Maven_TestNGIntegration/RestAPITest.java
@@ -0,0 +1,17 @@
+package Maven.Maven_TestNGIntegration;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");	
+	}
+}
---
+++ src/test/java/Maven/Maven_TestNGIntegration/SeleniumTest.java
@@ -0,0 +1,18 @@
+package Maven.Maven_TestNGIntegration;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void BrowserAUtomation()
+	{
+		System.out.println("BrowserAUtomation");	
+	}
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");	
+	}
+	
+	
+}
